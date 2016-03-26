843bffa628249aa1a62079037406f4449504f24c
JustinJonsson <junk.jonsson@gmail.com>
2016-03-26 12:38:06 -0400

    hide & display login & board

--- www/bingo/js/bingo.js
+++ www/bingo/js/bingo.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+
+  $("#loginform").submit(function(e){
+    $("#loginrow").hide();
+    $("#boardrow").show();
+  });
+
+  function init(){
+    $("#loginrow").show();
+    $("#boardrow").hide();
+  }
   
+  init();
 })
